b179ae65899ae932a3e23088c5486c64e74c5ff5
angel alvarez <angel.alvarez@quadit.mx>
2019-11-07 19:54:56 -0600

    [ADD],Nuevo modulo que se encargara de realizar la creacion de las ordenes de compras que se almacenan en el servidor FTP

---
+++ orders_planning/__openerp__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Orders Planning ',
+    'version': '9.0.1.0.0',
+    'depends': [
+        'mail',
+        'sale',
+        'purchase',
+        'account',
+        'sh_message',
+        'ftp_ediversa',
+        'ediversa_orders'
+    ],
+    'author': 'Quadit, S.A. de C.V.',
+    'description': 'eDiversa FTP',
+    'website': 'https://www.quadit.mx',
+    'data': [
+    'views/order_planning_view.xml'
+    ],
+    'demo': [],
+    'installable': True
+}
---
+++ orders_planning/models/order_planning.py
@@ -0,0 +1,63 @@
+#-*- coding: utf-8 -*-
+
+
+from openerp import _, api, fields, models, exceptions
+
+
+
+class AccountInvoice(models.Model):
+    _name = 'ediversa.order'
+    _inherit ='ediversa.order'
+
+    @api.model
+    def generar_orden(self, id=None):
+        print "###  ------------ Crear orden de compra ---------"
+        ftp_obj = self.env['ediversa.ftp']
+        ftp_ids = ftp_obj.search([])
+        conn = ftp_ids.test()
+        change_name = ftp_ids.cambiar_nombre()
+
+        if change_name:
+            print "Cambiaron los nombres"
+        else:
+            print "No hay cambios realizados"
+
+        document = ftp_ids.archivos()
+        doc = open('archivos.txt','r')
+        st =""
+        ban = False
+        #en este for se estan obteniendo todos los archivos en la carpeta Orders
+        #en el servidor FTP y este manera va ejecutar el codigo de ediversa.order
+        #para asi crear una orden de compra una a una
+        #al final del for se moveran todos los documentos a la caperta de Records
+
+        for linea in doc.readlines():
+            print linea
+            st=linea
+            file = open(st, 'wb')
+            conn.retrbinary('RETR %s' % st, file.write)
+            file.close()
+            file = open(st, 'r')
+            vals = file.read()
+            ediversa_obj = self.env['ediversa.order']
+            ediversa = ediversa_obj.search([])
+            attach_obj  = self.env['it.attachment']
+            attach_obj.create({
+            'datas': vals,
+            'name': 'Test mimetype txt',
+            'datas_fname': 'file.txt',
+            'mimetype': 'text/plain',
+            'folder_id': self.folder_b.id,
+            })
+            print attach_obj
+
+
+
+
+
+
+
+
+
+
+
