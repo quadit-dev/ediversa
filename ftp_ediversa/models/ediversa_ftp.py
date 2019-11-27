0275bacc88bcb5f2bf965977dfeda2ba5e572027
angel alvarez <angel.alvarez@quadit.mx>
2019-11-27 12:09:53 -0600

    [FIX],Correcion linea 71 permiso del archivo

--- ftp_ediversa/models/ediversa_ftp.py
+++ ftp_ediversa/models/ediversa_ftp.py
@@ -68,7 +68,7 @@ class ediversaFTP(models.Model):
             data=[]
             res={}
             conexion.dir(data.append)
-            doc = open('archivos.txt','r+')
+            doc = open('archivos.txt','w+')
             for f in data:
                 if f.endswith('txt'):
                     ff= f.split(" ")[-1]
